bfe99f703749d997c3f9bf8575a7ecd639a6985f
Gorbachev Vitalij <VGorbachev@bell-main.bellintegrator.ru>
2016-10-24 13:54:42 +0300

    Загрузка сайта

---
+++ index.php
@@ -0,0 +1,194 @@
+﻿<!DOCTYPE html>
+<html lang="en"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="http://bootstrap-3.ru/assets/ico/favicon.ico">
+    <title>R-Keeper™</title>
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.css" rel="stylesheet">
+    <link href="/css/carousel.css" rel="stylesheet">
+	<style>
+	#myCarousel img{
+		margin:auto;
+		max-height:800px;
+	}
+	
+	#myCarousel
+	{
+		margin-bottom: 50px;
+	}
+	
+
+	</style>
+</head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+	<!-- Menu -->
+    <div class="container">
+		<div class="row">
+		<h1></h1>
+			<div class="navbar navbar-default navbar-static" role="navigation">
+			  <div class="container">
+				<div class="navbar-header">
+				  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only">Stark company</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				  </button>
+				  <a class="navbar-brand" href="#">Stark company</a>
+				</div>
+				<div class="navbar-collapse collapse">
+				  <ul class="nav navbar-nav">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#about">О компании</a></li>
+					<li><a href="#contact">Контакты</a></li>
+					<li class="dropdown">
+					  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Подразделения <b class="caret"></b></a>
+					  <ul class="dropdown-menu">
+						<li><a href="#"></a></li>
+						<li><a href="#">R-Keeper™ 7</a></li>
+						<li><a href="#">Видеонаблюдение</a></li>
+						<li><a href="#">Звук</a></li>
+						<li class="divider"></li>
+						<li class="dropdown-header">Ссылки</li>
+						<li><a href="#">Separated link</a></li>
+						<li><a href="#">One more separated link</a></li>
+					  </ul>
+					</li>
+				  </ul>
+				</div>
+			  </div>
+			</div>		
+		</div>
+	</div>
+
+  
+      <div class="container-fluid">
+		<div class="row">
+
+			<!-- Carousel -->
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+			  <ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+			  </ol>
+			  <div class="carousel-inner">
+				<div class="item active">
+				  <img src="/img/eMenu.jpg"  alt="First slide">
+				  <div class="container">
+					<div class="carousel-caption text-center">
+					  <h1>Rkeeper</h1>
+					  <p>Программный комплекс R-Keeper™ - профессиональная система автоматизации для предприятий общественного питания: ресторанов, кафе, баров и других заведений, как одиночных так и сетевых. </p>
+					  <p><a class="btn btn-lg btn-primary" href="#" role="button">Подробнее:</a></p>
+					</div>
+				  </div>
+				</div>
+				<div class="item">
+				  <img src="/img/eMenu1.jpg"  alt="Second slide">
+				  <div class="container">
+					<div class="carousel-caption">
+					  <h1>Видеонаблюдение для бизнеса.</h1>
+					  <p>Система видеонаблюдения – это комплекс технических средств, первостепенной задачей которого является обеспечение визуального контроля за охраняемой территорией.</p>
+					  <p><a class="btn btn-lg btn-primary" href="#" role="button">Подробнее:</a></p>
+					</div>
+				  </div>
+				</div>
+				<div class="item">
+				  <img src="/img/eMenu2.jpg" alt="Third slide">
+				  <div class="container">
+					<div class="carousel-caption">
+					  <h1>Настройка звукового оборудования</h1>
+					  <p>В мире аудио профессиональное звуковое оборудование всегда составляло особую категорию, серьезно отличаясь от hi-fi и hi-end техники не только расширенными функциональными и коммутационными возможностями, но и самим характером звучания, который очень сильно зависит от назначения системы.</p>
+					  <p><a class="btn btn-lg btn-primary" href="#" role="button">Подробнее:</a></p>
+					</div>
+				  </div>
+				</div>
+			  </div>
+			  <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+			  <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+			</div>
+			<!-- /.carousel -->
+			
+			
+
+    <div class="container marketing">      
+      <div class="row">
+        <div class="col-lg-4">
+          <img src="/img/140/1.jpg" style="width: 140px; height: 140px;" class="img-circle"  alt="140x140">
+          <h2>Rkeeper</h2>
+          <p>С R-Keeper™ ресторатору доступны все необходимые инструменты управления рестораном, складом и производством, а также инновационные технологические решения по организации эффективной работы персонала ресторана и управлению лояльностью гостей: мобильные терминалы официанта на базе Apple iPod Touch, виртуальная карта гостя, электронное меню на планшете iPad, кассовые станции с дополнительным экраном для гостя, интеллектуальная система событийного видеоконтроля за кассовой зоной, система автоматического резервирования столов, CRM-система, система удаленного мониторинга работы ресторана, система автоматизации службы доставки, система автоматизации склада и другие. </p>
+          <p><a class="btn btn-default" href="#" role="button">Подробнее »</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img src="/img/140/2.jpg" style="width: 140px; height: 140px;" class="img-circle"  alt="140x140">
+          <h2>Видеонаблюдение</h2>
+          <p>В настоящее время HD системы видеонаблюдения с аналоговой передачей полностью сменили устаревший формат. Технологически современные системы обладают неоспоримым преимуществом и рядом новых возможностей. Есть много причин для перехода на новые форматы.
+Одним из основных преимуществ формата AHD является дальность передачи сигнала. В данном случае гарантированная дальность по коаксиальному кабелю составляет 500 метров.
+Далее следует разрешение. На сегодняшний день системы видеонаблюдения формата AHD способны обеспечить разрешающую способность 1,3 и 2 Мп.
+Следующим немаловажным фактором является гибридность. То есть возможность совместимости AHD формата с традиционным аналогом.
+И ключевая особенность, это стоимость системы видеонаблюдения формата AHD. На данный момент она является вполне бюджетной и соизмерима с традиционными аналоговыми системами.
+Более детальную информацию о новых аналогах вы найдете в статье "AHD, CVI, TVI: 10 причин переходить на новый аналог".</p>
+          <p><a class="btn btn-default" href="#" role="button">Подробнее »</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img src="/img/140/3.jpg" style="width: 140px; height: 140px;" class="img-circle"  alt="140x140">
+          <h2>Звук</h2>
+          <p>Представляем нескольких ведущих производителей, чье звуковое оборудование вы можете приобрести в наших салонах. Mackie - американская компания, которая удерживает невероятно высокий уровень популярности своего звукового оборудования благодаря пониманию нужд рынка, оригинальным технологиям и законченной продуктовой линейке. TAPCO by Mackie - это доступное, качественное звуковое оборудование со множеством профессиональных характеристик. В начале 2000-х произошло второе рождение этой торговой марки, с которой началась карьера Грега Мэки, легендарного разработчика профессионального звукового оборудования. Ни один музыкальный магазин невозможно представить без продукции Yamaha - признанного лидера на рынке музыкальных инструментов. </p>
+          <p><a class="btn btn-default" href="#" role="button">Подробнее »</a></p>
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+	  
+	  
+      <hr class="featurette-divider">
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Rkeeper 7. <span class="text-muted">It'll blow your mind.</span></h2>
+          <p class="lead">С R-Keeper™ ресторатору доступны все необходимые инструменты управления рестораном, складом и производством, а также инновационные технологические решения по организации эффективной работы персонала ресторана и управлению лояльностью гостей: мобильные терминалы официанта на базе Apple iPod Touch, виртуальная карта.</p>
+        </div>
+        <div class="col-md-5">
+          <img src="/img/500/1.png" class="featurette-image img-responsive"  alt="500x500">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+      <div class="row featurette">
+        <div class="col-md-5">
+          <img src="/img/500/2.jpg" class="featurette-image img-responsive"  alt="500x500">
+        </div>
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Видеонаблюдение. <span class="text-muted">See for yourself.</span></h2>
+          <p class="lead">В настоящее время HD системы видеонаблюдения с аналоговой передачей полностью сменили устаревший формат. Технологически современные системы обладают неоспоримым преимуществом и рядом новых возможностей. Есть много причин для перехода на новые форматы..</p>
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Звук. <span class="text-muted">Checkmate.</span></h2>
+          <p class="lead">Представляем нескольких ведущих производителей, чье звуковое оборудование вы можете приобрести в наших салонах. Mackie - американская компания, которая удерживает невероятно высокий уровень популярности своего звукового оборудования благодаря пониманию нужд рынка.</p>
+        </div>
+        <div class="col-md-5">
+          <img src="/img/500/3.jpg" class="featurette-image img-responsive"  alt="500x500">
+        </div>
+      </div>
+      <hr class="featurette-divider">
+
+	  
+    </div>
+
+	
+	</div></div>	
+	
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="/js/bootstrap.js"></script>
+  </body>
+</html>
